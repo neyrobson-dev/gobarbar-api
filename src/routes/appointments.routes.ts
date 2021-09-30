924d40d10d1dffaf9127c0f3d19649a78e3cb1bd
Ney Robson <ney.gama.external@k2partnering.com>
2021-09-30 10:09:17 -0300

    First commit

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,31 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+const appointmentsRouter = Router();
+
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const paserdDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(paserdDate);
+
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({ message: 'This appointment is already booked.' });
+  }
+
+  const appointment = appointmentsRepository.create({ provider, date: paserdDate });
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
