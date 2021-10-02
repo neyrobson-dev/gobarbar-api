894662ad683f99138ced87528bb415166b84a2d1
Ney Robson <ney.gama.external@k2partnering.com>
2021-10-02 10:13:11 -0300

    Entity e EntityRepository

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,27 +1,30 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 
-const appointmentsRepository = new AppointmentsRepository();
-
-appointmentsRouter.get('/', (request, response) => {
-  const appointments = appointmentsRepository.all();
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
 });
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
 
     const paserdDate = parseISO(date);
 
-    const createAppointmentService = new CreateAppointmentService(appointmentsRepository);
+    const createAppointmentService = new CreateAppointmentService();
 
-    const appointment = createAppointmentService.execute({ provider, date: paserdDate });
+    const appointment = await createAppointmentService.execute({
+      provider,
+      date: paserdDate,
+    });
 
     return response.json(appointment);
   } catch (err: any) {
